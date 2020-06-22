0eca76670beefa6a4abe9d4e3ea20d1b539f3efe
travis Lawson <travislawclimb@gmail.com>
2020-06-22 20:53:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,45 @@
-class Shoe
+# class Shoe
   
-  attr_accessor :color, :size, :material, :condition
   
-  attr_reader :brand 
+  
+  
+# end   
 
-  def initialize(brand)
-    @brand = brand
-  end   
-  
-  def cobble()
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end   
-  
-  
-  
-end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Shoe
+  
+#   attr_accessor :color, :size, :material, :condition
+  
+#   attr_reader :brand 
+
+#   def initialize(brand)
+#     @brand = brand
+#   end   
+  
+#   def cobble()
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end   
+  
+# end
+
+# Shoe.nike 
