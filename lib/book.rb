4a5d6cb6756019ac214c4dec84f5b3c8ad37cc0f
travis Lawson <travislawclimb@gmail.com>
2020-06-22 21:39:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,13 +2,12 @@ class Book
   
   attr_accessor :genre, :author, :page_count
   
+  attr_reader :title 
+  
   def initialize(title)
     @title = title 
   end   
   
-  def title 
-    @title
-  end   
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
