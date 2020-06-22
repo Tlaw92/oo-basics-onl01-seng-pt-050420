4902e99a047d3d6195c12e1c2e9bf843f18066b1
travis Lawson <travislawclimb@gmail.com>
2020-06-22 21:48:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,12 @@
 class Shoe
   
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand
   end   
   
-  def condition
-    puts "tattered"
-  end 
+
   
 end   
 
